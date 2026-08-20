7910711b0cc80b117673abb0774b320847cd50d6
HNKuragama <heshanhnk@gmail.com>
2026-08-20 23:51:13 +0530

    Navigation bar added

--- Frontend/Oil Mart Browse/src/App.jsx
+++ Frontend/Oil Mart Browse/src/App.jsx
@@ -1,36 +1,14 @@
 import ProductGrid from "./Components/ProductGrid.jsx";
+import Navbar from "./Components/navbar.jsx";
 
 function App() {
-  const items = [
-    "Engine Oil",
-    "Brake Fluid",
-    "Coolant",
-    "Transmission Fluid",
-    "Power Steering Fluid",
-  ];
-
-  const types = [
-    "Synthetic Oil",
-    "Conventional Oil",
-    "High-Mileage Oil",
-    "Diesel Engine Oil",
-    "Racing Oil",
-  ];
-
-  const prices = [
-    "$30 - $50",
-    "$10 - $20",
-    "$15 - $25",
-    "$25 - $40",
-    "$50 - $100",
-  ];
-
   const handleSelectItem = (item) => {
     console.log("Selected item:", item);
   };
   
   return (
     <div>
+      <Navbar />
       <ProductGrid />
     </div>
   );
